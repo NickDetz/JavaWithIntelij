227a2c502e444cbbbed55079c71548c20a624407
Deters <nicholasdeters47@live.com>
2021-02-18 10:27:33 -0800

    More fun with streams in java going to continue so it will be a good referrence

--- streamsinjava.java
+++ streamsinjava.java
@@ -7,13 +7,27 @@ public class streamsinjava {
 
     public static void main(String[] args) {
         System.out.println("Steams in java");
-
+        // example
         List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
         List<String> filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
 
 
         Random random = new Random();
         random.ints().limit(10).forEach(System.out::println);
+
+        List<String>strings1 = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+        List<String> filtered1 = strings1.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
+
+        System.out.println("Filtered List: " + filtered1);
+        String mergedString = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(", "));
+        System.out.println("Merged String: " + mergedString);
+
+        String[] names = {"Al", "Ankit", "Kushal", "Brent", "Sarika", "amanda", "Hans", "Shivika", "Sarah"};
+        Arrays.stream(names)	// same as Stream.of(names)
+                .filter(x -> x.startsWith("A"))
+                .sorted()
+                .forEach(System.out::println);
+
 
     }
 
